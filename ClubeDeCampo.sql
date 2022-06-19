c7fa45f40710de3e41d98be5fc29435ca9d1136d
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2022-06-19 18:19:00 -0300

    first commit

---
+++ ClubeDeCampo.sql
@@ -0,0 +1,28 @@
+CREATE TABLE tb_caixa
+
+CREATE TABLE tb_cidade
+
+CREATE TABLE tb_conta_receber
+
+CREATE TABLE tb_convidado
+
+CREATE TABLE tb_dependente
+
+CREATE TABLE tb_entrada_saida
+
+CREATE TABLE tb_estado
+
+CREATE TABLE tb_exame_medico
+
+CREATE TABLE tb_local
+
+CREATE TABLE tb_local_socio
+
+CREATE TABLE tb_exame_medico
+
+CREATE TABLE tb_mensalidade
+
+CREATE TABLE tb_local_socio
+
+CREATE TABLE tb_tipo_titulo
+  
