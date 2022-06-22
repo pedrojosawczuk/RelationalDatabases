35d870022c99e2bf11869906e9e76ee6f333269b
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2022-06-22 13:42:22 -0300

    change: add start and increment sequences

--- ClubeDeCampo.sql
+++ ClubeDeCampo.sql
@@ -33,7 +33,9 @@ CREATE TABLE tb_tipo_titulo (
   val_mensalidade NUMBER(9,2) NOT NULL,
   CONSTRAINT pk_tipo_titulo PRIMARY KEY (cod_tipo_titulo)
 );
-CREATE SEQUENCE sq_tipo_titulo;
+CREATE SEQUENCE sq_tipo_titulo
+  START WITH 0
+  INCREMENT BY 1;
 
 CREATE TABLE tb_local (
   cod_local NUMBER(6,0) NOT NULL,
@@ -42,7 +44,9 @@ CREATE TABLE tb_local (
   val_locacao NUMBER(9,2) NOT NULL,
   CONSTRAINT pk_local PRIMARY KEY (cod_local)
 );
-CREATE SEQUENCE sq_local;
+CREATE SEQUENCE sq_local
+  START WITH 0
+  INCREMENT BY 1;
 
 CREATE TABLE tb_estado (
   cod_estado CHAR(2) NOT NULL,
@@ -77,7 +81,9 @@ CREATE TABLE tb_socio (
   CONSTRAINT fk_socioXtipo_titulo FOREIGN KEY (cod_tipo_titulo) REFERENCES tb_tipo_titulo (cod_tipo_titulo),
   CONSTRAINT fk_socioXcidade FOREIGN KEY (cod_cidade) REFERENCES tb_cidade (cod_cidade)
 );
-CREATE SEQUENCE sq_socio;
+CREATE SEQUENCE sq_socio
+  START WITH 0
+  INCREMENT BY 1;
 CREATE INDEX idx_socio ON tb_socio (nome);
 
 CREATE TABLE tb_local_socio (
@@ -92,7 +98,9 @@ CREATE TABLE tb_local_socio (
   CONSTRAINT fk_local_socioXlocal FOREIGN KEY (cod_local) REFERENCES tb_local (cod_local),
   CONSTRAINT fk_local_socioXsocio FOREIGN KEY (cod_socio) REFERENCES tb_socio (cod_socio)
 );
-CREATE SEQUENCE sq_local_socio;
+CREATE SEQUENCE sq_local_socio
+  START WITH 0
+  INCREMENT BY 1;
 CREATE INDEX idx_local_socio1 ON tb_local_socio (cod_socio);
 CREATE INDEX idx_local_socio2 ON tb_local_socio (dt_reserva);
 
@@ -106,7 +114,9 @@ CREATE TABLE tb_medico (
   CONSTRAINT pk_medico PRIMARY KEY (cod_medico),
   CONSTRAINT fk_medicoXcidade FOREIGN KEY (cod_cidade) REFERENCES tb_cidade (cod_cidade)
 );
-CREATE SEQUENCE sq_medico;
+CREATE SEQUENCE sq_medico
+  START WITH 0
+  INCREMENT BY 1;
 
 CREATE TABLE tb_conta_receber (
   cod_conta_receber NUMBER(6,0) NOT NULL,
@@ -122,7 +132,9 @@ CREATE TABLE tb_conta_receber (
   CONSTRAINT fk_conta_receberXsocio FOREIGN KEY (cod_socio) REFERENCES tb_socio (cod_socio),
   CONSTRAINT fk_conta_receberXlocal_socio FOREIGN KEY (cod_local_socio) REFERENCES tb_local_socio (cod_local_socio)
 );
-CREATE SEQUENCE sq_conta_receber;
+CREATE SEQUENCE sq_conta_receber
+  START WITH 0
+  INCREMENT BY 1;
 CREATE INDEX idx_conta_receber1 ON tb_conta_receber (cod_socio);
 CREATE INDEX idx_conta_receber2 ON tb_conta_receber (dt_vencimento);
 
@@ -138,7 +150,9 @@ CREATE TABLE tb_mensalidade (
   CONSTRAINT pk_mensalidade PRIMARY KEY (cod_mensalidade),
   CONSTRAINT fk_mensalidadeXSocio FOREIGN KEY (cod_socio) REFERENCES tb_socio (cod_socio)
 );
-CREATE SEQUENCE sq_mensalidade;
+CREATE SEQUENCE sq_mensalidade
+  START WITH 0
+  INCREMENT BY 1;
 CREATE INDEX idx_mensalidade ON tb_mensalidade (cod_socio);
 
 CREATE TABLE tb_caixa (
@@ -156,7 +170,9 @@ CREATE TABLE tb_caixa (
   CONSTRAINT fk_caixaXlocal_socio FOREIGN KEY (cod_local_socio) REFERENCES tb_local_socio (cod_local_socio),
   CONSTRAINT fk_caixaXsocio FOREIGN KEY (cod_socio) REFERENCES tb_socio (cod_socio)
 );
-CREATE SEQUENCE sq_caixa;
+CREATE SEQUENCE sq_caixa
+  START WITH 0
+  INCREMENT BY 1;
 
 CREATE TABLE tb_convidado (
   cod_convidado NUMBER(9,0) NOT NULL,
@@ -170,7 +186,9 @@ CREATE TABLE tb_convidado (
   CONSTRAINT pk_convidado PRIMARY KEY (cod_convidado),
   CONSTRAINT fk_convidadoXsocio FOREIGN KEY (cod_socio) REFERENCES tb_socio (cod_socio)
 );
-CREATE SEQUENCE sq_convidado;
+CREATE SEQUENCE sq_convidado
+  START WITH 0
+  INCREMENT BY 1;
 CREATE INDEX idx_convidado1 ON tb_convidado (nome);
 CREATE INDEX idx_convidado2 ON tb_convidado (cod_socio);
 
@@ -187,7 +205,9 @@ CREATE TABLE tb_dependente (
   CONSTRAINT pk_dependente PRIMARY KEY (cod_dependente),
   CONSTRAINT fk_dependenteXsocio FOREIGN KEY (cod_socio) REFERENCES tb_socio (cod_socio)
 );
-CREATE SEQUENCE sq_dependente;
+CREATE SEQUENCE sq_dependente
+  START WITH 0
+  INCREMENT BY 1;
 CREATE INDEX idx_dependente1 ON tb_dependente (nome);
 CREATE INDEX idx_dependente2 ON tb_dependente (cod_socio);
 
@@ -204,7 +224,9 @@ CREATE TABLE tb_entrada_saida (
   CONSTRAINT fk_entrada_saidaXdependente FOREIGN KEY (cod_dependente) REFERENCES tb_dependente (cod_dependente),
   CONSTRAINT fk_entrada_saidaXconvidado FOREIGN KEY (cod_convidado) REFERENCES tb_convidado (cod_convidado)
 );
-CREATE SEQUENCE sq_entrada_saida;
+CREATE SEQUENCE sq_entrada_saida
+  START WITH 0
+  INCREMENT BY 1;
 CREATE INDEX idx_entrada_saida1 ON tb_entrada_saida (cod_socio);
 CREATE INDEX idx_entrada_saida2 ON tb_entrada_saida (cod_dependente);
 CREATE INDEX idx_entrada_saida3 ON tb_entrada_saida (cod_convidado);
@@ -226,7 +248,9 @@ CREATE TABLE tb_exame_medico (
   CONSTRAINT fk_exame_medicoXconvidado FOREIGN KEY (cod_convidado) REFERENCES tb_convidado (cod_convidado),
   CONSTRAINT fk_exame_medicoXmedico FOREIGN KEY (cod_medico) REFERENCES tb_medico (cod_medico)
 );
-CREATE SEQUENCE sq_exame_medico;
+CREATE SEQUENCE sq_exame_medico
+  START WITH 0
+  INCREMENT BY 1;
 CREATE INDEX idx_exame_medico1 ON tb_exame_medico (cod_socio);
 CREATE INDEX idx_exame_medico2 ON tb_exame_medico (cod_dependente);
 CREATE INDEX idx_exame_medico3 ON tb_exame_medico (cod_convidado);
