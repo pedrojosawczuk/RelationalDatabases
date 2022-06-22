d2ca110393c319fa9c2b1486e89098a6f94734bc
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2022-06-22 13:29:14 -0300

    change: add drop tables

--- ClubeDeCampo.sql
+++ ClubeDeCampo.sql
@@ -11,6 +11,21 @@ DROP SEQUENCE sq_dependente;
 DROP SEQUENCE sq_entrada_saida;
 DROP SEQUENCE sq_exame_medico;
 
+DROP TABLE tb_exame_medico;
+DROP TABLE tb_entrada_saida;
+DROP TABLE tb_dependente;
+DROP TABLE tb_convidado;
+DROP TABLE tb_caixa;
+DROP TABLE tb_mensalidade;
+DROP TABLE tb_conta_receber;
+DROP TABLE tb_medico;
+DROP TABLE tb_local_socio;
+DROP TABLE tb_socio;
+DROP TABLE tb_cidade;
+DROP TABLE tb_estado;
+DROP TABLE tb_local;
+DROP TABLE tb_tipo_titulo;
+
 CREATE TABLE tb_tipo_titulo (
   cod_tipo_titulo NUMBER(6,0) NOT NULL,
   desc_tipo_titulo VARCHAR2(25) NOT NULL,
