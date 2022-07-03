680530df52bf4abb2fea7ee8486525b84c5f98dc
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2022-07-03 15:29:09 -0300

    change: clube_de_campo change drop table order

--- clube_de_campo/drop_table.sql
+++ clube_de_campo/drop_table.sql
@@ -1,14 +1,27 @@
 DROP TABLE tb_exame_medico;
+
 DROP TABLE tb_entrada_saida;
+
 DROP TABLE tb_dependente;
+
 DROP TABLE tb_convidado;
+
 DROP TABLE tb_caixa;
+
 DROP TABLE tb_mensalidade;
+
 DROP TABLE tb_conta_receber;
+
 DROP TABLE tb_medico;
+
 DROP TABLE tb_local_socio;
+
 DROP TABLE tb_socio;
+
 DROP TABLE tb_cidade;
+
 DROP TABLE tb_estado;
+
 DROP TABLE tb_local;
+
 DROP TABLE tb_tipo_titulo;
